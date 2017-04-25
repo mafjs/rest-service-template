ec632d4771626216b981c4a3d90ed49ba74ee130
alekzonder <alekzonder@gmail.com>
2017-04-25 19:43:34 +0700

    chg: deps: upgrade

--- src/rest/responseHelpers.js
+++ src/rest/responseHelpers.js
@@ -1,19 +1,4 @@
 module.exports = function (rest) {
 
-    rest.responseHelpers.sendFindResult = function (result, requestQuery, api) {
-        var metadata = {
-            resultset: {
-                count: result.docs.length,
-                total: result.total,
-                limit: requestQuery.limit,
-                offset: requestQuery.offset
-            }
-        };
-
-        var docs = api.clearSystemFields(result.docs);
-
-        this.result(docs, metadata);
-    };
-
     return rest;
 };
